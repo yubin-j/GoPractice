725ccbae8cb96550ac233d7dfacda1b194074a45
yubin-j <developersurfer@gmail.com>
2021-01-13 22:36:51 +0900

    If with a Twist

--- main.go
+++ main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+func canIDrink(age int) bool {
+	/*
+		variable expression
+		if문안에서만 사용할 수 있는 변수를 생성할 수 있으며
+		아래와 같이 변수를 생성 후 ;으로 구분을 해주면 된다.
+		자바스크립트 for문의 초기화 구문이라고 보면 된다.
+	*/
+	if koreanAge := age + 1; koreanAge < 18 {
+		return false
+	}
+	return true
+}
+
 func main() {
-	fmt.Println("Hello Word")
+	fmt.Println(canIDrink(16))
 }
