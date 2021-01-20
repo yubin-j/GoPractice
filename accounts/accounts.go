85a995a37ab9442fc4e8f54cf33e32c2627c03da
yubin-j <developersurfer@gmail.com>
2021-01-20 22:50:23 +0900

    Finishing Up

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,87 +1,65 @@
 package accounts
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
-/*
-Account struct
-Account struct의 경우 지금은 Account로 하여 이 파일을 임포트 하는 곳에서 접근하도록 해둔다.
-Account struct의 owner와 balance는 현재 소문자로 시작하여 private한 변수이고
-만일 이를 Owner, Balance로 작성 시 외부에서도 접근이 가능한 public 변수가 된다.
-즉 이 public과 private한 성질을 결정짓는 요인은
-변수명이나 함수명 들이 대문자로 시작하면 public한 것이고 소문자로 시작하면 private한 것이다.
-*/
+//Account struct
 type Account struct {
 	owner   string
 	balance int
 }
 
-//err관련 변수의 경우 변수 이름시작이 err로 되야 한다고 golint에서 알려준다.
-//변수명으로 어떤 종류의 변수인지 한번에 알 수 있으므로 보기 좋다.
 var errNoMoney = errors.New("Can't withdraw you are poor")
 
-/*
-NewAccount create Account
-Account struct의 owner와 balance가 private변수 이므로 seter와 같은 함수를 작성해줘야 한다.
-여기서는 Account struct의 참조값을 출력함으로서 생성자(constructor)라고 볼 수 있다.
-
-반환해주는 account struct의 주소값을 *Account형식 즉 account struct의 값을 출력해준다.
-메모리에 복사가 일어나지 않으므로 효율적으로 동작한다고 볼 수 있다.
-*/
+//NewAccount create account (constructor)
 func NewAccount(owner string) *Account {
 	account := Account{owner: owner, balance: 0}
 	return &account
 }
 
-/*
-Deposit x amount on your account
-Go에서 메서드는 func와는 다른 것이다.
-형식은 func와 비슷하지만 func와 함수 이름 사이에 리시버라는 것이 들어간다.
-아래 메서드에서 리시버는 (a Account)이다.
-이는 이 함수를 Account의 메서드로 사용하겠다는 것이다. 리시버를 넣게 되면 메서드로 동작하게 된다고 생각하면 된다.
-리시버의 구성은 다음과 같다 => (struct의 정보를 담을 이름 struct)
-아래 메서드에서 struct의 정보를 담을 이름은 a이고 stuct는 Account이다.
-참고로 이름은 struct의 첫글자를 소문자로 한 것으로 시작해야 한다.
-Account의 첫글자의 소문자는 a이므로 아래와 같이 작성한 것이다.
-메서드 안에서는 a가 Account의 정보를 담고 있으므로
-a.balance와 같은 구문으로 값에 접근 할 수 있다.
-마치 자바스크립트의 this처럼 말이다.
-
-자 추가적으로 리시버를 (a Account)로 해서 값을 넣어봤더니 값이 들어가지 않았다.
-왜냐하면 포인터 때문인데 위에 생성자를 보면 반환하는 값이 account의 주소값이다.
-즉 우리가 이를 받을때도 주소값을 그대로 받는 것이 아닌 그 값이 바라보고 있는 struct를 가져와야 한다.
-그래서 아래에 적혀있는 것 처럼 리시버를 (a *Account)로 하여 값을 가져와야 한다.
-
-이러한 리시버를 가리켜 포인터 리시버라고 한다.
-*/
+//Deposit on account
 func (a *Account) Deposit(amount int) {
 	a.balance += amount
 }
 
-/*
-Balance show balance
-아래에서 사용한 리시버는 포인터 리시버가 아닌 일반 리시버인데
-main.go에서 같은 방식으로 메서드를 호출하는데 왜 일반 리시버도 동작을 할까?
-Go는 기본적으로 값을 복사하여 전달하는데 아래와 같이 타입이 포인터 타입이 아닌 경우에 값을 복사하여 전달하고
-포인터 타입인 경우에만 참조값을 전달한다.
-아래의 경우 값을 조작할 필요 없이 불러오기만 하면 되기에 일반 타입을 사용한 리시버를 이용한 것이다.
-*/
+//Balance show from account
 func (a Account) Balance() int {
 	return a.balance
 }
 
-/*
-Withdraw from your account
-Go에서는 Exception관련 기능(try - except, try - catch)이 없어서 직접 관리를 해줘야 한다.
-*/
+//Withdraw from account
 func (a *Account) Withdraw(amount int) error {
 	if a.balance < amount {
-		//return errors.New("Can't withdraw you are poor")
-		//위와 같이 errors.New함수를 사용하여 에러를 출력해준다.
-		//추가적으로 리턴하는 곳에 새로운 에러를 만들어서 사용해도 되지만
-		//전역에 error를 선언해두고 값을 불러오는 것이 더 깔끔하다고 한다.
 		return errNoMoney
 	}
 	a.balance -= amount
-	return nil //nil은 null과 같은 것이라고 보면 된다.
-	//error가 반환타입으로 되어 있기에 정상적인 경우 nil값을 출력함으로서 함수의 반환타입을 만족시킨다.
+	return nil
+}
+
+//ChangeOwner of the account
+func (a *Account) ChangeOwner(newOwner string) {
+	a.owner = newOwner
+}
+
+//Owner of the account
+func (a Account) Owner() string {
+	return a.owner
+}
+
+/*
+Account struct를 사용하는 경우
+println과 같은 곳에서 Account struct를 가리키는 변수 자체를 출력하는 형식을 지정할 수 있다.
+
+자바스크립트에서를 예로 들면 함수 선언 후 해당 주소값을 가지고 있는 변수를 콘솔로그에 출력 시
+함수 전체 코드가 문자열로 나오는 것을 볼 수 있다.
+이는 함수 메서드의 toString()을 통해서 나오는 결과인데 이 toString 메서드를 다른 함수로 변경 시
+변경된 함수에서 출력해주는 대로 나오는 것을 볼 수 있다.
+
+Go에서도 위의 예와 같은 것을 할 수 있으며 아래 코드들처럼 작성을 하면 된다.
+메서드명은 String으로 해야하며 리시버로는 포인터가 아닌 값을 복사해서 오게 하면 된다.
+*/
+func (a Account) String() string {
+	return fmt.Sprint(a.Owner(), "'s account.\nHas: ", a.Balance())
 }
--- main.go
+++ main.go
@@ -2,33 +2,14 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/yubin-j/GoPractice/accounts"
 )
 
 func main() {
-	/*
-	   struct에 대해서 연습.
-	   accounts폴더에 accounts.go파일을 생성하여 accounts package를 구성.
-
-	*/
 	account := accounts.NewAccount("yubin")
-	//account.balance = 1000  balance가 undefined라고 에러가 난다.
-	//이는 balance가 private한 값이기 때문이며 이를 수정하기 위해서는 메서드를 만들어 사용해야한다.
 
-	//메서드는 아래와 같이 .으로 전개하여 부르면 된다.
 	account.Deposit(1000)
 
-	//아래의 경우 에러가 출력되지만 Go에서는 이를 가지고 아무런 동작을 취하지 않는다.
-	//그래서 다음 코드들 처럼 직접 에러를 처리하는 코드르 작성해야 한다.
-	account.Withdraw(1200)
-
-	err := account.Withdraw(1200)
-	if err != nil {
-		//log.Fatalln은 err를 출력해주고 프로그램 실행을 즉시 종료시키는 함수이다.
-		log.Fatalln(err)
-	}
-
-	fmt.Println(account.Balance()) //받아온 값이 참조값을 바라보는 값을 받아오게 되어 출력형식에 이를 알려주는 &가 붙는다.
+	fmt.Println(account)
 }
