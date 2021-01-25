6642b7f6fb1246539b788cbf82b72432a64038d6
yubin-j <developersurfer@gmail.com>
2021-01-25 22:38:19 +0900

    Add Method

--- main.go
+++ main.go
@@ -7,12 +7,15 @@ import (
 )
 
 func main() {
-	dictionary := mydict.Dictionary{"first": "First word"}
-	definition, err := dictionary.Search("second")
-
+	dictionary := mydict.Dictionary{}
+	err := dictionary.Add("hello", "Greeting")
 	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println(definition)
+	}
+	definition, _ := dictionary.Search("hello")
+	fmt.Println("found: hello, definition:", definition)
+	err = dictionary.Add("hello", "Greeting")
+	if err != nil {
+		fmt.Println(err)
 	}
 }
--- mydict/mydict.go
+++ mydict/mydict.go
@@ -10,6 +10,7 @@ type의 경우 메서드를 가질 수 있다.
 type Dictionary map[string]string
 
 var errNotFound = errors.New("Not Found")
+var errWordExists = errors.New("Word Exists")
 
 /*
 Search struct의 메소드와 동일하게 리시버를 받아줘야 한다.
@@ -21,4 +22,20 @@ func (d Dictionary) Search(word string) (string, error) {
 	}
 	return "", errNotFound
 
+}
+
+/*
+Add 이곳에서 리서버가 포인터 리시버가 되어야 할 줄 알았으나
+일반 리시버를 사용해도 잘 동작한다.
+왜 그러하냐면 Dictionary 타입이 담고 있는 map타입은 기본적으로 포인터를 이용하기 때문이다.
+*/
+func (d Dictionary) Add(word, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case errNotFound:
+		d[word] = def
+	case nil:
+		return errWordExists
+	}
+	return nil
 }
